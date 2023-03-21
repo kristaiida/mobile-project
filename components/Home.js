0ee01dda219357802e716dfad9e65ed0f61e9eb8
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-03-21 15:31:36 +0200

    tested API, moved API key to gitignore

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,44 @@
+import { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { API_KEY } from '../Api_Key';
 
 export default function Home() {
 
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': API_KEY,
+                'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+            }
+        };
+        fetch('https://tasty.p.rapidapi.com/feeds/list?size=5&timezone=%2B0700&vegetarian=false&from=0', options)
+        .then(response => response.json())
+        .then(
+            (result) => {
+                setRecipes(result.results)
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <ScrollView>
+        {recipes.map(recipe => (
+            <View>
+                <Text>{recipe.type}</Text>
+            </View>
+        ))}
+      </ScrollView>
     </View>
   );
-
-};
+}
 
 const styles = StyleSheet.create({
   container: {
