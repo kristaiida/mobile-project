21d6f66c453a61ad7e8b6946421b7c1f21d29470
kristaiida <92725476+kristaiida@users.noreply.github.com>
2023-03-21 12:27:54 +0200

    Bottom tab nav and icons for it

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Home from './components/Home.js';
+import Search from './components/Search.js';
+import Page from './components/Page.js';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to RecipePal!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search-circle' : 'search-circle-outline';
+            } else if (route.name === 'Page') {
+              iconName = focused ? 'heart-sharp' : 'heart-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'turquoise',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{tabBarStyle: {display:"none"}}}/>
+        <Tab.Screen name="Search" component={Search}/>
+        <Tab.Screen name="Page" component={Page}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+)};
