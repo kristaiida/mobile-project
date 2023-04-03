5027b39212231b0e131855f0a4f60f05c2d63a5e
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-04-03 10:31:07 +0300

    fixed

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,7 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import CarouselScreen from '../components/Carousel';
 import Header from '../components/Header';
-import { Text, View } from 'react-native';
 import styles from '../styles/styles';
 
 export default function Favorites() {
