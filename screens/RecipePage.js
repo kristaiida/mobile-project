8cae3693ad69aa8576aeb802b661a1564f4cd480
iRekila <ilkka.rekila@gmail.com>
2023-04-18 11:18:48 +0300

    removed carousel, added description

--- screens/RecipePage.js
+++ screens/RecipePage.js
@@ -14,10 +14,13 @@ export default function RecipePage({ route }) {
         </View>
         <View style={styles.textContainer}>
           <Text style={styles.titleRC}>{recipe.name}</Text>
-          <Text style={styles.descriptionRC}>{recipe.description}</Text>
+          {recipe.description ? (
+            <Text style={styles.descriptionRC}>{recipe.description}</Text>
+          ) : (
+            <Text style={styles.descriptionRC}>No description available for this recipe.</Text>
+          )}
         </View>
       </View>
     </View>
   );
-
 };
