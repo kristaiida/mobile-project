2546253acac5cbf4652bdf21fd17e4db6e79499b
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-04-23 09:41:53 +0300

    search results only include singular recipes

--- screens/Search.js
+++ screens/Search.js
@@ -9,10 +9,10 @@ import styles from "../styles/styles";
 const Search = () => {
   const [searchPhrase, setSearchPhrase] = useState("");
   const [clicked, setClicked] = useState(false);
-  const [stillData, setStillData] = useState();
+  const [stillData, setStillData] = useState([]);
 
   useEffect(() => {
-    const criteria = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q='
+    const criteria = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=50&q='
     const options = {
       method: 'GET',
       headers: {
@@ -20,43 +20,45 @@ const Search = () => {
         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
       }
     };
-    
     fetch(criteria + searchPhrase, options)
       .then(response => response.json())
       .then(
         (result) => {
-            setStillData(result.results)
+          const filteredResults = [];
+          for (let i = 0; i < result.results.length; i++) {
+            if (!result.results[i].recipes) {
+              filteredResults.push(result.results[i]);
+            }
+          }
+          setStillData(filteredResults);
         },
         (error) => {
-            console.log(error);
+          console.log(error);
         }
+      );
+    }, [searchPhrase]);
+
+    return (
+      <View>
+        <SafeAreaView style={styles.root}>
+          <SearchBar
+            searchPhrase={searchPhrase}
+            setSearchPhrase={setSearchPhrase}
+            clicked={clicked}
+            setClicked={setClicked}
+          />
+          { !stillData ? (
+            <ActivityIndicator size="large" />
+            ) : (
+              <List
+                searchPhrase={searchPhrase}
+                data={stillData}
+                setClicked={setClicked}
+              />
+            ) }
+        </SafeAreaView>
+      </View>
     );
-}, [searchPhrase]);
-
-  return (
-    <View>
-      <SafeAreaView style={styles.root}>
-
-        <SearchBar
-          searchPhrase={searchPhrase}
-          setSearchPhrase={setSearchPhrase}
-          clicked={clicked}
-          setClicked={setClicked}
-        />
-        {!stillData ? (
-          <ActivityIndicator size="large" />
-        ) : (
-          
-            <List
-              searchPhrase={searchPhrase}
-              data={stillData}
-              setClicked={setClicked}
-            />
-          
-        )}
-      </SafeAreaView>
-    </View>
-  );
 };
 
 export default Search;
