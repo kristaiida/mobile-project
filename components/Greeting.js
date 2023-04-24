fb344dbf6790449766583b4598d65480df2bbf49
Jussi Wedman <juswedma@gmail.com>
2023-04-24 18:37:26 +0300

    added comments to the code

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,22 +1,25 @@
 import { useState, useEffect } from 'react';
 
 export function UseGreeting() {
-  const [greeting, setGreeting] = useState('');
 
-  useEffect(() => {
+  // useStates
+  const [greeting, setGreeting] = useState(''); 
+
+  // useEffect hook to set the greeting based on current time
+  useEffect(() => { 
     const date = new Date();
     const hours = date.getHours();
 
-    if (hours >= 6 && hours < 12) {
+    if (hours >= 6 && hours < 12) { // set greeting to 'Good morning' between 6 AM and 12 PM
       setGreeting('Good morning');
-    } else if (hours >= 12 && hours < 17) {
+    } else if (hours >= 12 && hours < 17) { // set greeting to 'Good afternoon' between 12 PM and 5 PM
       setGreeting('Good afternoon');
-    } else if (hours >= 17 && hours < 24) {
+    } else if (hours >= 17 && hours < 24) { // set greeting to 'Good evening' between 5 PM and midnight
       setGreeting('Good evening');
-    } else {
+    } else { // set greeting to 'Go back to sleep' for other times
       setGreeting('Go back to sleep');
     }
-  }, []);
+  }, []); 
 
-  return greeting;
+  return greeting; 
 }
