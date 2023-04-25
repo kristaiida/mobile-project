6d8b8a33dbf1edc83b137e5f0e0f34f5bb7bbc35
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-04-25 20:55:43 +0300

    tweaked registering

--- screens/Register.js
+++ screens/Register.js
@@ -1,86 +1,81 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
-import { child, push, ref, update, query, equalTo, off } from 'firebase/database';
+import { push, ref, update, get } from 'firebase/database';
 import { db, USERS_REF } from '../firebase/Config';
 import styles from '../styles/styles';
-import { onValue } from 'firebase/database';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Register() {
-    const [newUsername, setNewUsername] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const navigation = useNavigation();
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const navigation = useNavigation();
 
-    const handleRegisterAndNavigate = () => {
-        addNewUser();
+  const handleRegisterAndNavigate = async () => {
+    const newUserCreated = await addNewUser();
+    if (newUserCreated) {
         navigation.navigate('Main');
-        
-   }
+    }
+  };
 
-    const addNewUser = () => {
-        if (newUsername.trim() === "" || newPassword.trim() === "") {
-            Alert.alert('Error', 'Please enter a valid username and password');
+  const addNewUser = async () => {
+    if (newUsername.trim() === "" || newPassword.trim() === "") {
+        Alert.alert('Error', 'Please enter a valid username and password');
+        return;
+    }
+
+    const usersRef = ref(db, USERS_REF);
+    const snapshot = await get(usersRef);
+
+    if (snapshot.exists()) {
+        const userData = snapshot.val();
+        const usernames = Object.keys(userData).map(key => userData[key].username);
+        if (usernames.includes(newUsername.trim())) {
+            Alert.alert('Error', 'Username already taken');
             return;
         }
+    }
 
-        const usersRef = ref(db, USERS_REF);
-        const usernameQuery = query(usersRef, equalTo('username', newUsername.trim()));
-        
-        return new Promise((resolve, reject) => {
-            const handleSnapshot = (snapshot) => {
-                if (snapshot.exists()) {
-                    Alert.alert('Error', 'Username already taken');
-                    resolve();
-                } else {
-                    const newUserAccount = {
-                        username: newUsername.trim(),
-                        password: newPassword.trim()
-                    };
-                    const newUserAccountKey = push(usersRef).key;
-                    const updates = {};
-                    updates[USERS_REF + newUserAccountKey] = newUserAccount;
-                    setNewUsername('');
-                    setNewPassword('');
-                    update(ref(db), updates).then(() => {
-                        resolve();
-                    }).catch((error) => {
-                        reject(error);
-                    });
-                }
-            };
-            const handleError = (error) => {
-                reject(error);
-            };
-            const querySubscription = onValue(usernameQuery, handleSnapshot, handleError);
-            return () => {
-                off(querySubscription);
-            };
-        });
+    const newUserAccount = {
+        username: newUsername.trim(),
+        password: newPassword.trim()
     };
+    const newUserAccountKey = push(usersRef).key;
+    const updates = {};
+    updates[USERS_REF + newUserAccountKey] = newUserAccount;
+    setNewUsername('');
+    setNewPassword('');
 
-    return (
-        <View style={styles.registerContainer}>
-            <Text>Register</Text>
-            <View>
-                <TextInput
-                    placeholder='Username'
-                    value={newUsername}
-                    onChangeText={setNewUsername}
-                />
-            </View>
-            <View>
-                <TextInput
-                    placeholder='Password'
-                    value={newPassword}
-                    onChangeText={setNewPassword}
-                    secureTextEntry={true}
-                />
-            </View>
-            <View>
-                <TouchableOpacity onPress={handleRegisterAndNavigate}>
-                    <Text>Register</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+    try {
+        await update(ref(db), updates);
+    } catch (error) {
+        console.log(error);
+        Alert.alert('Error', 'An error occurred');
+    }
+  };
+
+  return (
+    <View style={styles.registerContainer}>
+      <Text>Register</Text>
+      <View>
+        <TextInput
+          placeholder='Username'
+          value={newUsername}
+          onChangeText={setNewUsername}
+        />
+      </View>
+      <View>
+        <TextInput
+          placeholder='Password'
+          value={newPassword}
+          onChangeText={setNewPassword}
+          secureTextEntry={true}
+        />
+      </View>
+      <View>
+        <TouchableOpacity onPress={handleRegisterAndNavigate}>
+          <Text>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
