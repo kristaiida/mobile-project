4ffac5a56482ae5e26a8fbd061db602f08b4b1b5
Jussi Wedman <juswedma@gmail.com>
2023-04-26 11:02:27 +0300

    styling + emojis depening of the time

--- styles/styles.js
+++ styles/styles.js
@@ -317,7 +317,12 @@ trashIcon: {
   greetingText: {
     fontWeight: "bold",
     textAlign: "center",
-
+    fontSize: 20,
+  },
+  greetingText2 : {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 15,
   },
   loginContainer: {
     flex: 1,
