da2a6635225e17c7856dd3ab2ef2b4d9c8d142fc
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-05-02 12:29:31 +0300

    filters work stna

--- components/List.js
+++ components/List.js
@@ -4,28 +4,11 @@ import styles from "../styles/styles";
 import RecipeCard from "./RecipeCard";
 
 const List = ({ searchPhrase, tags, setClicked, data }) => {
-  const formattedSearchPhrase = searchPhrase.trim().replace(/\s/g, "").toUpperCase();
-
-  const filteredData = data.filter((item) => {
-    const formattedItemName = item.name?.trim().replace(/\s/g, "")?.toUpperCase();
-    const formattedItemDetails = item.details?.trim().replace(/\s/g, "")?.toUpperCase();
-
-    // Check if the item's name or details include the search phrase
-    const matchesSearchPhrase = formattedItemName?.includes(formattedSearchPhrase) || formattedItemDetails?.includes(formattedSearchPhrase);
-
-    // Check if the item's tags include all of the selected tags
-    const hasSelectedTags = tags.length === 0 || tags.every(tag => item.tags?.includes(tag.name));
-
-    return matchesSearchPhrase && hasSelectedTags;
-  });
-
-  console.log(filteredData); // Add this line to log filteredData
 
   const renderItem = ({ item }) => {
     const itemName = item.name?.trim();
-    const itemDetails = item.details?.trim();
 
-    if (!itemName && !itemDetails) {
+    if (!itemName) {
       return null;
     }
 
@@ -45,14 +28,12 @@ const List = ({ searchPhrase, tags, setClicked, data }) => {
           setClicked(false);
         }}
       >
-        {filteredData ? (
           <FlatList
-            data={filteredData}
+            data={data}
             renderItem={renderItem}
             keyExtractor={(item) => item.id}
             extraData={[searchPhrase, tags]}
           />
-        ) : null}
       </View>
     </SafeAreaView>
   );
