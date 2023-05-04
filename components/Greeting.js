6e47305eb942babb97efc98e1a7e52766b4d3c9e
Noora Turpeinen <turpeinen.n@hotmail.com>
2023-05-04 11:30:59 +0300

    emojis to greeting.js

--- components/Greeting.js
+++ components/Greeting.js
@@ -5,6 +5,7 @@ export function UseGreeting() {
   // useStates
   const [greeting, setGreeting] = useState(''); 
   const [meal, setMeal] = useState('');
+  const [emoji, setEmoji] = useState('');
 
   // useEffect hook to set the greeting based on current time
   useEffect(() => { 
@@ -12,33 +13,39 @@ export function UseGreeting() {
     const hours = date.getHours();
 
     if (hours >= 6 && hours < 10) { // set greeting to 'Good morning' between 6 AM and 12 PM
-      setGreeting('☀️ Good Morning');
+      setGreeting('Good Morning');
       setMeal('breakfast');
+      setEmoji('🍳');
     } else if (hours >= 10 && hours < 11) {
-      setGreeting('☀️ Good Morning');
+      setGreeting('Good Morning');
       setMeal('snacks');
+      setEmoji('🥨');
     } else if (hours >= 11 && hours < 14) {
       if (hours >= 11 && hours < 12) {
-        setGreeting('☀️ Good Morning');
+        setGreeting('Good Morning');
       } else {
-        setGreeting('😎 Good Afternoon');
+        setGreeting('Good Afternoon');
       }
       setMeal('lunch');
+      setEmoji('🥗');
     } else if (hours >= 14 && hours < 17) { // set greeting to 'Good afternoon' between 12 PM and 5 PM
-      setGreeting('😎 Good Afternoon');
+      setGreeting('Good Afternoon');
       setMeal('snacks');
+      setEmoji('🥨');
     } else if (hours >= 17 && hours < 21) { // set greeting to 'Good evening' between 5 PM and midnight
       if (hours >= 17 && hours < 18) {
-        setGreeting('😎 Good Afternoon');
+        setGreeting('Good Afternoon');
       } else {
-        setGreeting('😊 Good Evening');
+        setGreeting('Good Evening');
       }
       setMeal('dinner');
+      setEmoji('🍝');
     } else {
-      setGreeting('🥂 Up at this hour,');
+      setGreeting('Up at this hour,');
       setMeal('drinks');
+      setEmoji('🍹');
     }
   }, []); 
 
-  return { greeting, meal };
+  return { greeting, meal, emoji };
 }
